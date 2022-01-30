d587c9bed866fa418c3deda31d883dc70e64cd97
Muhammad Saqlain <96041833+RanaSaqlain@users.noreply.github.com>
2022-01-30 13:41:19 +0500

    Week Plan 4
    Customer Pannel Week Plan 4

---
+++ ShoppingBazar/Customers/footer.php
@@ -0,0 +1,23 @@
+  <footer class="footer pt-0  col-10">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-6">
+            <div class="copyright text-center  text-lg-left  text-muted">
+              &copy; 2022 <a href="" class="font-weight-bold ml-1" target="_blank">Shopping Bazar</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+              <li class="nav-item">
+                <a href="#" class="nav-link" target="_blank">Shopping Bazar</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://ranasaqlain.github.io/ShoppingBazar/Intro.html" class="nav-link" target="_blank">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link" target="_blank">Blog</a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+      </footer>
---
+++ ShoppingBazar/Customers/leftsidebar.php
@@ -0,0 +1,48 @@
+
+ <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light "
+ id="sidenav-main" >
+    <div class="scrollbar-inner"  >
+      <!-- Brand -->
+      <div class="sidenav-header  align-items-center bg-white col-12" >
+        <a class="navbar-brand" href="index.php" >
+          <img src="../assets/img/logo/logo_wbg.png" class="navbar-brand-img" alt="..." style="width:90%;min-height:5rem;">
+        </a>
+         <a class="navbar-link" href="index.php" >
+        <span style="color:#e65b0b">Shop<span style="color: #0397ab;" >ping</span><span  style="color: #0266bd;">Bazar</span></span></a>
+      </div>
+      <hr>
+      <div class="navbar-inner "   >
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+          <!-- Nav items -->
+          <ul class="navbar-nav">
+           
+              <li class="nav-item">
+              <a class="nav-link" href="Paymenthistory.php">
+                <i class="ni ni-tv-2 text-primary"></i>
+                <span class="nav-link-text">Payment History</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Wishlist.php">
+                <i class="ni ni-tv-2 text-primary"></i>
+                <span class="nav-link-text">Wishlist</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="order-details.php">
+                <i class="ni ni-tv-2 text-primary"></i>
+                <span class="nav-link-text">Curent Order Details</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php">
+                <i class="ni ni-planet text-orange"></i>
+                <span class="nav-link-text">Logout</span>
+              </a>
+            </li>
+           
+        </div>
+      </div>
+    </div>
+  </nav>
