14c58b50eba346c63d35c0233551704f18f01cfb
Baqir Ali Jaffri <96041958+BaqirJaffri110@users.noreply.github.com>
2022-01-31 11:39:48 +0500

    Add files via upload
    Putting data of week plan #4 ,  Customers Pannel

---
+++ ShoppingBazar/Customers/Wishlist.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Shopping Bazar Team">
+  <title>ShoppingBazar | Users </title>
+  <!-- Favicon -->
+  <link rel="icon" href="../assets/img/brand/favicon.png" type="image/png">
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+  <!-- Icons -->
+  <link rel="stylesheet" href="../assets/vendor/nucleo/css/nucleo.css" type="text/css">
+  <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <!-- Page plugins -->
+  <!-- Argon CSS -->
+  <link rel="stylesheet" href="../assets/css/argon.css?v=1.2.0" type="text/css">
+</head>
+
+<body style="width:98.9%;">
+  <!-- Sidenav -->
+ <?php
+
+include("leftsidebar.php");
+ ?>
+  <!-- Main content -->
+  <div class="main-content row" id="panel" >
+    <!-- Topnav -->
+    <?php
+
+include("topnav.php");
+ ?>
+
+
+      <div class="container text-center">
+
+        <h2 class="text-center">WishList</h2>
+
+<div class="row">
+  <div class="col-xs-4  ml-3">
+     <div class="card" style="width: 18rem;">
+  <img src="Mobile.jpg" class="card-img-top" alt="Mobile Image">
+  <div class="card-body">
+    <h5 class="card-title">A New Brand Mobile</h5>
+    <p class="card-text">Available In Just 200 $</p>
+<button type="button" class="btn btn-primary">Add To Cart</button>
+  </div>
+</div>
+</div>
+  <div class="col-xs-4 ml-3">
+
+
+        <div class="card" style="width: 18rem;">
+  <img src="Watch.png" class="card-img-top" alt="Watch Image">
+  <div class="card-body">
+    <h5 class="card-title">New Design Watch</h5>
+    <p class="card-text">Available In Just 75 $</p>
+<button type="button" class="btn btn-primary">Add To Cart</button>
+  </div>
+</div>
+</div>
+    <div class="col-xs-4 ml-3">
+
+<div class="card" style="width: 18rem;">
+  <img src="Headphones.png" class="card-img-top" alt="Headphones Image">
+  <div class="card-body">
+    <h5 class="card-title">Headphones with amazing sound</h5>
+    <p class="card-text">Available In Just 150 $</p>
+<button type="button" class="btn btn-primary">Add To Cart</button>
+  </div>
+</div>      
+    
+  </div>
+</div>
+       
+
+      </div>
+    
+    
+      <!-- Footer -->
+    <?php
+    include "footer.php";
+
+    ?>
+    </div>
+
+   <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
+  <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+  <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+  <!-- Optional JS -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+  <!-- Argon JS -->
+  <script src="../assets/js/argon.js?v=1.2.0"></script>
+</body>
+
+</html>
