4ecdbea45506d5e74edc6f267c62e10b64788701
Sharjeel Ahmed <96041860+SharjeelAhmed111@users.noreply.github.com>
2022-02-27 18:53:13 +0500

    Week Plan 5
    Created Shop Main Page Design And Develop
    
    Co-Authored-By: Muhammad Saqlain <96041833+RanaSaqlain@users.noreply.github.com>
    Co-Authored-By: Baqir Ali Jaffri <96041958+BaqirJaffri110@users.noreply.github.com>

---
+++ ShoppingBazar/categories.php
@@ -0,0 +1,199 @@
+<?php 
+require_once('top.php');
+require_once 'db.php';
+require_once "function.php";
+    $srched = array();
+$cat_id=mysqli_real_escape_string($con,$_GET['id']);
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if( isset($_POST["srchbtn"]) )
+    {
+        $srch = $_POST['srch'];
+  
+      $srched=  get_searched_product($con , $srch);
+            
+    }
+}
+
+?>
+     <div class="body__overlay"></div>
+        <!-- Start Offset Wrapper -->
+        <div class="offset__wrapper">
+            <!-- Start Search Popap -->
+            <div class="search__area">
+                <div class="container" >
+                    <div class="row" >
+                        <div class="col-md-12" >
+                            <div class="search__inner">
+                                <form action="" method="post">
+                                    <input placeholder="Looking for ....  " name="srch" type="text" required="">
+                                    <button type="submit" name="srchbtn"></button>
+                                </form>
+
+                                <div class="search__close__btn">
+                                    <span class="search__close__btn_icon"><i class="zmdi zmdi-close"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Search Popap -->
+           <?php include("Cart.php"); ?>
+        </div>
+        <!-- End Offset Wrapper -->
+                     <?php 
+
+                    if ($srched != null) {
+                            include("srchedproducts.php");
+                    }
+                   ?>
+            <!-- Searched product area end -->
+        <!-- Start Bradcaump area -->
+        <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(#) no-repeat scroll center center / cover ;">
+            <div class="ht__bradcaump__wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="bradcaump__inner">
+                                <nav class="bradcaump-inner">
+                                  <a class="breadcrumb-item" href="index.html">Home</a>
+                                  <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
+                                  <span class="breadcrumb-item active">Products</span>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Bradcaump area -->
+        <!-- Start Product Grid -->
+        <section class="htc__product__grid bg__white ptb-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9  col-md-9 col-sm-12 col-xs-12">
+                        <div class="htc__product__rightidebar">
+                            <div class="htc__grid__top">
+                                <div class="htc__select__option">
+                                    <select class="ht__select">
+                                        <option>Default softing</option>
+                                        <option>Sort by popularity</option>
+                                        <option>Sort by average rating</option>
+                                        <option>Sort by newness</option>
+                                    </select>
+                                </div>
+                                
+                                <!-- Start List And Grid View -->
+                                <ul class="view__mode" role="tablist">
+                                    <li role="presentation" class="grid-view active"><a href="#grid-view" role="tab" data-toggle="tab"><i class="zmdi zmdi-grid"></i></a></li>
+                                    <li role="presentation" class="list-view"><a href="#list-view" role="tab" data-toggle="tab"><i class="zmdi zmdi-view-list"></i></a></li>
+                                </ul>
+                                <!-- End List And Grid View -->
+                            </div>
+                            <!-- Start Product View -->
+                            <div class="row">
+
+                                <div class="shop__grid__view__wrap ">
+                                    <div role="tabpanel" id="grid-view" class="single-grid-view tab-pane fade in active clearfix">
+                                        <!-- Start Single Product -->
+                                        <?php
+                            $get_products=get_products_cat($con,$cat_id);
+                            if ($get_products == null) {
+
+                                echo "<span style='margin-left:auto;margin-right:auto;'> No Product is added please  visit other products</span>";
+                            }
+                            foreach($get_products as $list)
+                            {
+
+                            ?>
+                            <!-- Start Single Category -->
+                            <div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+                                <div class="category">
+                                    <div class="ht__cat__thumb">
+                                        <a href="product-details.php">
+                                            <img src="<?php echo $list['product_Image'] ?>">
+                                        </a>
+                                    </div>
+                                    <div class="fr__hover__info">
+                                        <ul class="product__action">
+                                            <li><a href="wishlist.php"><i class="icon-heart icons"></i></a></li>
+
+                                            <li><a href="cart.php"><i class="icon-handbag icons"></i></a></li>
+
+                                            <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="fr__product__inner">
+                                        <h4><a href="product-details.php"><?php echo $list['product_Name'] ?></a></h4>
+                                        <ul class="fr__pro__prize">
+                                        
+                                            <li><?php echo $list['product_sprice']."$" ?></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        }                           
+
+                      ?>
+                        </div>
+                     
+               
+                                    <div role="tabpanel" id="list-view" class="single-grid-view tab-pane fade clearfix">
+                                             <?php
+                            $get_products=get_products_cat($con,$cat_id);
+                            foreach($get_products as $list)
+                            {
+
+                            ?>
+                                        <div class="col-xs-12">
+                                            <div class="ht__list__wrap">
+                                                <!-- Start List Product -->
+                                                <div class="ht__list__product">
+                                                    <div class="ht__list__thumb">
+                                                        <a href="product-details.html"><img src="<?php echo $list['product_Image'] ?>" alt="product images"></a>
+                                                    </div>
+                                                    <div class="htc__list__details">
+                                                        <h2><a href="product-details.html"><?php echo $list['product_Name'] ?> </a></h2>
+                                                        <ul  class="pro__prize">
+                                                            <li class="old__prize"></li>
+                                                            <li>$<?php echo $list['product_sprice']?></li>
+                                                        </ul>
+                                                        <ul class="rating">
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                        </ul>
+                                                        <p><?php echo $list['product_Description'] ?></p>
+                                                        <div class="fr__list__btn">
+                                                            <a class="fr__btn" href="cart.html">Add To Cart</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- End List Product -->
+                          
+                                            </div>
+                                        </div>
+                                        <?php
+                    }
+                    ?>
+                                    </div>
+                                                            
+                                </div>
+                            </div>
+                            <!-- End Product View -->
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+
+     <?php 
+     require('footer.php');
+ ?>
---
+++ ShoppingBazar/function.php
@@ -0,0 +1,56 @@
+<?php
+function get_products($con,$type='')
+{
+	$sql="SELECT * FROM `products`";
+	if ($type=='latest')
+	 {
+		$sql.="  where `product_Status`='1' order by product_id desc LIMIT 10";
+	}
+
+	
+	$res=mysqli_query($con,$sql);
+	$data=array();
+	while($row=mysqli_fetch_assoc($res))
+	{
+		$data[]=$row;
+	}
+	return $data;
+}
+function get_products_cat($con,$cat_id='')
+{
+	$sql="SELECT * FROM `products`";
+	if($cat_id!='')
+	{
+		$sql.=" inner join category on  products.C_ID = category.Category_ID where `Category_ID` ='$cat_id'";
+	}
+
+		$sql.=" order by product_id desc  LIMIT 7";
+	// print_r($sql);exit();
+	$res= mysqli_query($con,$sql);
+
+	$data=array();
+	while($row=mysqli_fetch_assoc($res))
+	{
+		$data[]=$row;
+
+	}
+	
+	return $data;
+}
+function get_searched_product($con,$srch)
+{
+  $sql = " SELECT products.* from products WHERE product_Name LIKE '%$srch%' OR product_Description LIKE '%$srch%' OR product_Color LIKE '%$srch%' or product_sprice LIKE '%$srch%'  LIMIT 8";
+  $res= mysqli_query($con,$sql);
+
+	$data=array();
+	while($row=mysqli_fetch_assoc($res))
+	{
+		$data[]=$row;
+
+	}
+	
+	return $data;
+
+
+}
+?>
